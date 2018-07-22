dfd40ab3af7df545f1753109eb19d7d2692128fb
Jose Cortez <jose.cortez@epn.edu.ec>
2018-07-21 19:37:41 -0500

    Cambios en la musica y el tiempo

--- VentanaMain/x.py
+++ VentanaMain/x.py
@@ -7,6 +7,13 @@ pygame.display.set_caption("Trigon Fusionado")
 reloj = pygame.time.Clock()
 y = y.Personaje((500, 400))
 JuegoTerminado = False
+#fuente de texto para ecribir en pantalla
+fuente1 = pygame.font.SysFont("Arial",20,True,False)
+#Escribir tiempo en pantalla
+tiempo=fuente1.render("Tiempo:",0,(255,255,255))
+
+#Inicio de musica de fondo
+pygame.mixer.Sound("MusicaFondo.wav").play()
 
 while JuegoTerminado == False:
 
@@ -17,6 +24,12 @@ while JuegoTerminado == False:
     y.manejoDeEvento(event)
     pantalla.fill(pygame.Color('black'))
     pantalla.blit(y.image, y.rect)
+    #contador (tiempo)
+    pantalla.blit(tiempo,(5,5))#mensaje tiempo
+    segundos = round(pygame.time.get_ticks()/1000)
+    segundos = str(segundos)
+    contador = fuente1.render(segundos,0,(0,0,230))
+    pantalla.blit(contador,(85,6))
 
     pygame.display.flip()
     reloj.tick(20)
